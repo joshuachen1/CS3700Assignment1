c92e12d358ebc31da3c68f21bee718a0ded04d76
Joshua Chen <joshuachen1@cpp.edu>
2018-10-04 21:12:22 -0700

    Introduced thread.join()

--- AverageArrayBarrier.java
+++ AverageArrayBarrier.java
@@ -14,30 +14,41 @@ public class AverageArrayBarrier {
         }
         @Override
         public void run() {
-            newArray[index] = avg(oldArray[index - 1], oldArray[index + 1]);
+            newArray[index - 1] = avg(oldArray[index - 1], oldArray[index + 1]);
         }
     }
     public void averageNeighbors() {
         final int numThreads = oldArray.length - 2;
         CyclicBarrier barrier = new CyclicBarrier(numThreads);
         Thread[] thread = new Thread[numThreads];
-        for (int i = 0; i < thread.length - 1; i++) {
-            thread[i] = new Thread(new NewElement(i+1));
+
+        for (int i = 0, j = 1; i < thread.length; i++, j++) {
+            thread[i] = new Thread(new NewElement(j));
         }
+
         for (int i = 0; i < thread.length; i++) {
             thread[i].start();
+        }
+
+        for (int i =0; i < thread.length; i++) {
+            try {
+                thread[i].join();
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread " + Thread.currentThread().getId() + ": INTERRUPTED");
+            }
         }
     }
     public void showNewArray() {
         for (int i = 0; i < newArray.length; i++) {
-            System.out.print(newArray[i]);
+            System.out.print(newArray[i] + " ");
         }
     }
     public static int avg (int a, int b) {
         return (a + b) / 2;
     }
     public static void main(String[] args) {
-        int[] oldArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] oldArr = {11, 31, 9, 32, 67, 29, 61, 7, 24, 41};
         AverageArrayBarrier test = new AverageArrayBarrier(oldArr);
         test.showNewArray();
     }
